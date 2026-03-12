5f7610383d5d12e945619a06825181605205b956
essspess <amit.kumar.malaker@gmail.com>
2026-03-12 16:08:11 +0600

    Implement Player Spawner

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,29 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 
-public class GameManager : MonoBehaviour // TODO
+public static class IntExtension
 {
+    public static void Times(this int self, Action<int> action)
+    {
+        for (int i = 0; i < self; ++i)
+        {
+            action(i);
+        }
+    }
+}
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject characterPrefab;
+    [SerializeField] List<Transform> spawnPoints;
+    [SerializeField] int numberOfFighters;
     void Start()
     {
-
+        // Start the game by spawning some fighters in the arena
+        numberOfFighters.Times(i =>
+        {
+            Instantiate(characterPrefab, spawnPoints[i].position, Quaternion.identity);
+        });
     }
 }
